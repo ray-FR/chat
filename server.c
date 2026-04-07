4eef0bf180c1ecc94793a49bbe444ab6b23b3a63
R.I <ares1345istaken@gmail.com>
2026-04-07 15:31:13 +0200

    Import server code from previous netcat project

---
+++ server.c
@@ -0,0 +1,115 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <poll.h>
+
+
+int nc_serv(char* port){
+    struct addrinfo hints, *gai, *ai;
+    int err;
+    int sfd; 
+    int acc;
+    
+    bzero(&hints, sizeof(hints));
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;
+    if ((err = getaddrinfo(NULL, port, &hints, &gai)) != 0){
+        fprintf(stderr, "./server: getaddrinfo [%s] %s", port, gai_strerror(err));
+        exit(EXIT_FAILURE);
+    } 
+    
+    for (ai = gai; ai != NULL; ai = ai->ai_next){
+        if ((sfd = socket(ai->ai_family, ai->ai_socktype, 0)) < 0){
+            perror("socket");
+            continue;
+        }
+        int v = 1;
+        if ((setsockopt(sfd, SOL_SOCKET, SO_REUSEADDR, &v, sizeof(int))) < 0){
+            perror("setsockopt");
+            exit(EXIT_FAILURE);
+        }
+        if (bind(sfd, ai->ai_addr, ai->ai_addrlen) < 0){
+            perror("bind");
+            continue;
+        }
+        printf("Server online!\n");
+
+        while(1){
+        if (listen(sfd, 8) < 0){
+            perror("listen");
+            continue;
+        }
+        printf("Waiting for a connection...\n");
+        if ((acc = (accept(sfd, ai->ai_addr, &ai->ai_addrlen))) < 0) {
+            perror("accept");
+            exit(EXIT_FAILURE);
+        }
+        printf("Connected!\n");}
+        break;
+    }
+    
+    if (ai == NULL){
+        fprintf(stderr, "Something went wrong\n");
+        exit(EXIT_FAILURE);
+    }
+    freeaddrinfo(gai);
+    close(sfd);
+    return acc;
+    
+    
+    
+}
+
+
+int main(int argc, char** argv){
+    int sfd; 
+    int len;
+    int ret;
+    
+    uint8_t buf[1024];
+    struct pollfd pfd[2];
+    if(argc == 1){
+        fprintf(stderr, "Usage: ./nc [HOST] PORT\n");
+        exit(EXIT_FAILURE);
+    }
+    if(argc > 3){
+        fprintf(stderr, "Usage: ./nc [HOST] PORT\n");
+        exit(EXIT_FAILURE);    
+    }
+    
+    if (argc == 2){
+       sfd = nc_serv(argv[1]); 
+    }
+    
+    
+    
+    // for (;;){
+        
+    //     // if ((ret = poll(pfd, 2, 20000)) < 0){
+    //     //     perror("poll");
+    //     // }
+
+    //     if (ret > 0){
+    //         bzero(&buf, sizeof(buf));
+            
+    //         if (pfd[0].revents & POLLIN){
+    //             if ((len = recv(sfd, buf, sizeof(buf), 0)) < 0){
+    //                 perror("recv");
+    //                 exit(EXIT_FAILURE);
+    //             }
+    //             fprintf(stderr, (char*) buf, len);
+                
+                
+    //         }
+    //     }
+    // }
+        
+    return 0;
+}
