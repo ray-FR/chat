12ffcfebc59f4a1891cc91456b1b983b29356b22
R.I <ares1345istaken@gmail.com>
2026-04-15 23:25:15 +0200

    Add error interpretor to client.py

--- client.py
+++ client.py
@@ -2,7 +2,18 @@ import socket
 import sys
 import select
 
-
+err_dict = {
+    "ERR! 00\n" : "Erreur côté serveur",
+    "ERR! 01\n" : "Erreur côté serveur",
+    "ERR! 02\n" : "Erreur côté serveur",
+    "ERR! 03\n" : "Erreur côté serveur",
+    "ERR! 10\n" : "Erreur côté serveur",
+    "ERR! 11\n" : "Erreur côté serveur",
+    "ERR! 12\n" : "Erreur côté serveur",
+    "ERR! 20\n" : "Erreur côté serveur",
+    "ERR! 21\n" : "Erreur côté serveur",
+     
+}
 
 if len(sys.argv) != 3:
     print(f"Error on arg length, expected 3 args, only have {len(sys.argv)}")
@@ -27,6 +38,9 @@ while True:
     if not serv_response:
         print("Cut by the host")
         sys.exit(1)
-
-    print(serv_response.decode())
+    decoded_serv_response = serv_response.decode()
+    if ((decoded_serv_response)) in err_dict:
+        print(f"{err_dict[decoded_serv_response]}, Error code: {decoded_serv_response}")
+    else:
+        print(decoded_serv_response)
 
